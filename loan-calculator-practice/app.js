c639d2b6b86804414948c6972819e402f303102e
Sarah Adewale <saraphynaadewale@gmail.com>
2023-01-04 04:44:02 +0100

    completed loan calculation

---
+++ loan-calculator-practice/app.js
@@ -0,0 +1,35 @@
+document.getElementById('loan-form').addEventListener('submit', calculateResults)
+
+//create the function
+function calculateResults(e){
+    console.log('This works')
+    const amount = document.getElementById('amount')
+    const interest = document.getElementById('interest')
+    const years = document.getElementById('years')
+    const monthlyPayment = document.getElementById('monthly-payment')
+    const totalPayment = document.getElementById('total-payment')
+    const totalInterest = document.getElementById('total-interest')
+
+    const principal = parseFloat(amount.value)
+    const calculatedInterest = parseFloat(interest.value) /100 / 12
+    const calculatedPayments = parseFloat(years.value) * 12
+
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments)
+    const monthly = (principal * x * calculatedInterest) / (x - 1)
+
+    //to check if the value you are getting from users input is a number
+    if(isFinite(monthly)){
+        monthlyPayment.value = monthly.toFixed(2)
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2)
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2)
+    }else{
+        console.log('Please input a value')
+
+        showError('Please check your numbers')
+    }
+
+
+
+    e.preventDefault()
+}
+
